8b78a400095c1d321f54365b9c5ecd6459e53a33
amanVar06 <98205162+amanVar06@users.noreply.github.com>
2022-12-17 21:10:01 +0530

    Initialized git repo

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Speech from "./components/Speech/Speech";
+
+const App = () => {
+  return <main>App</main>;
+};
+
+export default App;
---
+++ src/components/Speech/Speech.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from "react";
+import "./Speech.scss";
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const mic = new SpeechRecognition();
+
+mic.continuous = true;
+mic.interimResults = true;
+mic.lang = "en-US";
+
+const Speech = () => {
+  const [isListening, setIsListening] = useState(false);
+  const [note, setNote] = useState(null);
+  const [savedNotes, setSavedNotes] = useState([]);
+
+  useEffect(() => {
+    handleListen();
+  }, [isListening]);
+
+  const handleListen = () => {
+    if (isListening) {
+      mic.start();
+      mic.onend = () => {
+        console.log("continue..");
+        mic.start();
+      };
+    } else {
+      mic.stop();
+      mic.onend = () => {
+        console.log("Stopped Mic on Click");
+      };
+    }
+    mic.onstart = () => {
+      console.log("Mics on");
+    };
+
+    mic.onresult = (event) => {
+      const transcript = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+      console.log(transcript);
+      setNote(transcript);
+      mic.onerror = (event) => {
+        console.log(event.error);
+      };
+    };
+  };
+
+  const handleSaveNote = () => {
+    setSavedNotes((prevNotes) => [...prevNotes, note]);
+    setNote("");
+  };
+
+  return (
+    <>
+      <h1>Voice Notes</h1>
+      <div className="container">
+        <div className="box">
+          <h2>Current Note</h2>
+          {isListening ? (
+            <span>🎙 Listening...</span>
+          ) : (
+            <span>🛑 Click Start...</span>
+          )}
+          <button onClick={handleSaveNote} disabled={!note}>
+            Save the Note
+          </button>
+          <button onClick={() => setIsListening((prevState) => !prevState)}>
+            Start/Stop
+          </button>
+          <p>{note}</p>
+        </div>
+        <div className="box">
+          <h2>Notes</h2>
+          {savedNotes.map((n, index) => (
+            <p key={index}>{n}</p>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Speech;
